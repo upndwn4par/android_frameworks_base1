8f40437c3713df654dc408966c8cdabdc8232152
BigBrother1984 <stevewatersy@gmail.com>
2014-04-10 18:07:26 +0200

    Floating Window: Welcome back
    * Port squashed jellybean floating window to kitkat.
    
    Floating window contributors:
    @drcmda
    @beerbong
    @D4rKn3sSyS
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Merge notification manager service changes.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    Change-Id: If3d6338f657915754bd6cb9cb987df62c17aa6d0
    
    Conflicts:
    	services/java/com/android/server/NotificationManagerService.java

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -27,6 +27,7 @@ import android.app.IActivityManager;
 import android.app.INotificationManager;
 import android.app.ITransientNotification;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.ProfileGroup;
 import android.app.ProfileManager;
@@ -693,6 +694,7 @@ public class NotificationManagerService extends INotificationManager.Stub
     @Override
     public void registerListener(final INotificationListener listener,
             final ComponentName component, final int userid) {
+
         final int permission = mContext.checkCallingPermission(
                 android.Manifest.permission.SYSTEM_NOTIFICATION_LISTENER);
         if (permission == PackageManager.PERMISSION_DENIED)
