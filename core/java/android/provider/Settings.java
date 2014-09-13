65607fef64817307bae9d52ccc1ce3e2b36b9272
upndwn4par <upndwn4par@gmail.com>
2014-09-13 09:29:21 -0400

    Nav bar height/width customization (1/2) Based on this commit: https://github.com/OmniKang/android_frameworks_base/commit/e16f12b553a872e400a2ce8754255f75ead68b14
    Change-Id: I035c25934593aad85fe334f80c97ca54f8b05fec

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3783,6 +3783,18 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
 
         /**
+         * Navigation bar height when it is on protrait
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
+         * Navigation bar height when it is on landscape at the right
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
